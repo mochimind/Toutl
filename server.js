08e7c8d10746e6447254d891c0559177ea969efd
unknown <jxie@US6482357-X7005.clients.us.kworld.kpmg.com>
2015-02-25 17:53:09 -0800

    basic loading of landing page

--- server.js
+++ server.js
@@ -3,10 +3,14 @@
 var express = require('express');
 var gravatar = require('gravatar');
 
-var io = require('socket.io').listen(app.listen(port));
-var app = express();
-
 var port = 80;
+
+var app = express();
+var io = require('socket.io').listen(app.listen(port));
+
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res){ 
 	res.render('home'); 
